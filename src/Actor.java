af421d9cf738d596ef6dcacc38399d66eac0c5bb
Ben Morris <2266439@swansea.ac.uk>
2023-06-10 12:46:10 +0100

    Added all variables and constructors for player, monster and actor. Created a calculate players level method as well to process the computational problem. I have also made a few 'temporary' changes in the Item class as they will need to be populated with actual items to use in the game. I just want to firstly make sure all of this code works with the other objectives of the game, and then we can go about polishing off the details.

--- src/Actor.java
+++ src/Actor.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  * This class describes an Actor: a character in the game.
  * @author jackroberts
@@ -13,11 +16,10 @@ public abstract class Actor {
     private final int MINIMUM_LEVEL = 0;
     private final int MAXIMUM_LEVEL = 100;
     private final int MINIMUM_ARMORCLASS_LEVEL = 0;
-    private final int MAXIMUM_ARMORCLASS_LEVEL = level / 2;
     private final int MAXIMUM_HEALTH = 100;
-    public Actor(int level, String name, int armourClass, int health){
+    public Actor(int level, String name) {
 
-        if (level > MINIMUM_LEVEL && level < MAXIMUM_LEVEL){
+        if (level > MINIMUM_LEVEL && level < MAXIMUM_LEVEL) {
             this.level = level;
         } else {
             throw new IllegalArgumentException("Level must be between 0" +
@@ -31,20 +33,145 @@ public abstract class Actor {
             this.name = name;
         }
 
+        this.armourClass = calculateArmourClass(level);
         if (armourClass < MINIMUM_ARMORCLASS_LEVEL ||
-                armourClass > MAXIMUM_ARMORCLASS_LEVEL){
-            this.armourClass = armourClass;
+                armourClass > calculateArmourClass(level)) {
             throw new IllegalArgumentException("Armour level must be above 0" +
                     "and below the actor level divided by 2.");
-        } else {
-            this.armourClass = armourClass;
         }
 
-        if (health != MAXIMUM_HEALTH){
-            throw new IllegalArgumentException("Health must start at 100.");
-        } else {
-            health = MAXIMUM_HEALTH;
-            // Initiates health as 100 when an actor is created.
+        this.health = MAXIMUM_HEALTH;
+        // Sets health at 100 (the max) when constructing an actor
+    }
+        public int getLevel() {
+            return level;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getArmourClass() {
+            return armourClass;
+        }
+
+        public int getHealth() {
+            return health;
+        }
+
+        private int calculateArmourClass(int level) {
+        return Math.min(level / 2, 100);
+        }
+}
+
+    class Player extends Actor {
+        private int experience;
+        private ArrayList<Item> inventory;
+        private int gold;
+        private Item currentWeapon;
+        private int gainedXP;
+        private final int MINIMUM_EXP = 0;
+        private final int MINIMUM_GOLD = 0;
+
+        public Player(int level, String name) {
+            super(level, name);
+            this.experience = MINIMUM_EXP;
+
+            this.inventory = new ArrayList<Item>();
+
+            this.gold = MINIMUM_GOLD;
+
+            this.currentWeapon = null;
+
+            calculatePlayersLevel(level, experience, gainedXP);
+        }
+
+        // Getters and setters for player constructor
+        public int getExperience() {
+            return experience;
+        }
+
+        public void setExperience(int experience) {
+            this.experience = experience;
+        }
+
+        public ArrayList<Item> getInventory() {
+            return inventory;
+        }
+
+        public void setInventory(ArrayList<Item> inventory) {
+            this.inventory = inventory;
+        }
+
+        public int getGold() {
+            return gold;
+        }
+
+        public void setGold(int gold) {
+            this.gold = gold;
+        }
+
+        public Item getCurrentWeapon() {
+            return currentWeapon;
+        }
+
+        public void setCurrentWeapon(Item currentWeapon) {
+            this.currentWeapon = currentWeapon;
+        }
+
+        public void calculatePlayersLevel(int level, int experience,
+                                                 int gainedXP) {
+            final int MAX_LEVEL = 100;
+            final int MINIMUM_EXP = 0;
+            int requiredExp = (int) (3 * (level * 0.1) + 10);
+            if (level < MAX_LEVEL) {
+                experience += gainedXP;
+                if (experience > requiredExp) {
+                    level += 1;
+                    System.out.println("You have leveled up! You are now level "
+                            + level + ".");
+                } else {
+                    System.out.println("You gained " + gainedXP + " experience " +
+                            "points.");
+                }
+            } else if (experience < MINIMUM_EXP) {
+            throw new IllegalArgumentException("Experience cannot be " +
+                    "below 0.");
+            } else {
+                System.out.println("You have reached the maximum level." +
+                        "You have been awarded 100 gold.");
+                setGold(gold + 100);
+            }
         }
     }
+
+    class Monster extends Actor {
+    private Item loot;
+
+    public Monster(int level, String name) {
+        super(level, name);
+        this.loot = generateRandomItem();
+    }
+
+    private Item generateRandomItem() {
+        Random random = new Random();
+        Item item = new Item("Item name will go here " +
+                random.nextInt(100)); // This will need to be changed
+        // Depending on how many items we get
+        return item;
+    }
+
+    public Item getLoot() {
+        return loot;
+    }
+
+    public void setLoot(Item loot) {
+        this.loot = loot;
+    }
+    @Override
+    public String toString() {
+        return "Monster - Name: " + getName() + ", Level: " + getLevel() +
+                ", Loot: " + loot.getName();
+    }
 }
+
--- src/Item.java
+++ src/Item.java
@@ -5,4 +5,18 @@
  */
 public class Item {
 
+    private String name;
+
+    public Item(String s) {
+    } // To be used to help the actor class, as actor class needs this item,
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    // constructor
+
 }
