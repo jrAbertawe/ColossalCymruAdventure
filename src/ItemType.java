62bb0d3b2041a4391dcdf6aa50995c636ba16d0e
zachealy <1908931@swansea.ac.uk>
2023-06-14 11:39:18 +0100

    Changes to item class and addition of new weapons class.

---
+++ src/ItemType.java
@@ -0,0 +1,51 @@
+/**
+ * This file describes a collectible item rewarded after every battle.
+ */
+/**
+ * This enum describes a collectible item rewarded after every battle.
+ * 
+ * @author jackroberts
+ *
+ * @version 1.1
+ * @modified 2023-06-10
+ */
+public enum ItemType {
+  /** The sword item type. */
+  SWORD,
+  /** The potion item type. */
+  POTION,
+  /** The bara brith item type. */
+  BARA_BRITH,
+  /** The text book item type. */
+  TEXT_BOOK,
+  /** The spear item type. */
+  SPEAR,
+  /** The phone item type. */
+  PHONE,
+  /** Invalid item */
+  INVALID;
+
+  /**
+   * Returns the corresponding item type based off of the input. 
+   * 
+   * @param item the string representation of the item.
+   * @return the item type based on the input.
+   */
+  public static ItemType stringToItem(String item) {
+    if (item.equalsIgnoreCase("sword")) {
+      return SWORD;
+    } else if (item.equalsIgnoreCase("potion")) {
+      return POTION;
+    } else if (item.equalsIgnoreCase("bara brith")) {
+      return BARA_BRITH;
+    } else if (item.equalsIgnoreCase("text book")) {
+      return TEXT_BOOK;
+    } else if (item.equalsIgnoreCase("spear")) {
+      return SPEAR;
+    } else if (item.equalsIgnoreCase("phone")) {
+      return PHONE;
+    } else {
+      return INVALID;
+    }
+  }
+}
---
+++ src/Weapon.java
@@ -0,0 +1,49 @@
+/**
+ * The Weapon class represents a specific type of item that can inflict damage.
+ */
+/**
+ * The Weapon class represents a specific type of item that can inflict damage.
+ * It extends the Item class.
+ */
+public class Weapon extends Item {
+	private int weaponDamage;
+
+	/**
+	 * Constructs a new Weapon object with the specified type and weapon damage.
+	 * 
+	 * <p>
+	 * Side-effect sets the item type and sets weapon damage.
+	 * 
+	 * @param type         the type of the weapon
+	 * 
+	 * @param weaponDamage the damage value inflicted by the weapon
+	 */
+	public Weapon(ItemType type, int weaponDamage) {
+		super(type);
+		this.weaponDamage = weaponDamage;
+	}
+
+	/**
+	 * Returns the damage value inflicted by the weapon.
+	 * 
+	 * <p>
+	 * Side-effect free. Not referentially transparent.
+	 * 
+	 * @return the damage value inflicted by the weapon
+	 */
+	public int getWeaponDamage() {
+		return weaponDamage;
+	}
+
+	/**
+	 * Sets the damage value inflicted by the weapon.
+	 * 
+	 * <p>
+	 * Side-effect changes weapon damage.
+	 * 
+	 * @param weaponDamage the damage value inflicted by the weapon
+	 */
+	public void setWeaponDamage(int weaponDamage) {
+		this.weaponDamage = weaponDamage;
+	}
+}
