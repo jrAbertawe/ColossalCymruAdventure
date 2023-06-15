b2e26acd58f68354ff99f05d64a2c9d88f966886
Stuart <2156276@swansea.ac.uk>
2023-06-15 20:43:59 +0100

    Implementation of Movement Java class v0.1 - Untested

--- src/Movement.java
+++ src/Movement.java
@@ -11,4 +11,48 @@
  */
 public class Movement {
 
+    // Default constructor
+	public Movement() {
+	}
+	
+	public int movePlayer(int coordinate, char direction) {
+		
+		switch (direction) {
+			case 'n':	
+				if (coordinate <= 0) {
+					System.out.println("Can't go north!");
+				} else {
+					System.out.println("You have gone north.");
+					coordinate--;
+				}
+				break;
+			case 's':
+				if (coordinate >= 5) {
+					System.out.println("Can't go south!");
+				} else {
+					System.out.println("You have gone south.");
+					coordinate++;
+				}
+				break;
+			case 'w':
+				if (coordinate <= 0) {
+					System.out.println("Can't go west!");
+				} else {
+					System.out.println("You have gone west.");
+					coordinate--;
+				}
+				break;
+			case 'e':
+				if (coordinate >= 5) {
+					System.out.println("Can't go east!");
+				} else {
+					System.out.println("You have gone east.");
+					coordinate++;
+				}
+				break;
+			default:
+		}
+	
+		return coordinate;
+	}	
 }
