3446439c90e97f22293e935ce3db5d62aeaa5a77
Stuart <2156276@swansea.ac.uk>
2023-06-15 19:56:26 +0100

    Created an empty Movement Java class

---
+++ src/Movement.java
@@ -0,0 +1,14 @@
+/**
+ * Class to manage the movement of the player within the game
+ * <br>
+ * Methods included are:
+ * <pre>To be defined</pre>
+ * 
+ * @author Stuart Gadd
+ * @version 0.1
+ * @since 2023/06/15
+ * 
+ */
+public class Movement {
+
+}
