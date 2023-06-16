7ecfd53e481ed84f13cd8f3a2966831285a3ee35
Stuart <2156276@swansea.ac.uk>
2023-06-16 11:22:36 +0100

    Renamed Movement class to MovementManager and added error checking and constant variables

---
+++ src/MovementManager.java
@@ -0,0 +1,74 @@
+/**
+ * Class to manage the movement of the player within the game
+ * <br>
+ * Methods included are:
+ * <pre>To be defined</pre>
+ * 
+ * @author Stuart Gadd
+ * @version 0.1 - Initial draft of Movement method
+ * @version 0.2 - Added error checking, constant variables and renamed class to be in-line with project naming convention
+ * @since 2023/06/16
+ * 
+ */
+public class MovementManager {
+
+	private final int MIN_COORDINATE = 0;
+	private final int MAX_COORDINATE = 4;
+	
+    // Default constructor
+	public MovementManager() {
+	}
+	
+	public int movePlayer(int coordinate, char direction) {
+			
+		switch (direction) {
+			case 'n':	
+				if (coordinate <= MIN_COORDINATE) {
+					System.out.println("Can't go north!");
+				} else if (coordinate > MAX_COORDINATE){
+					System.out.println("FATAL ERROR DURING MOVING NORTH ACTION: Player is outside boundary of game!!! PlayerY coordinate is " + coordinate);
+					System.exit(0);
+				} else {
+					System.out.println("You have travelled north.");
+					coordinate--;
+				}
+				break;
+			case 's':
+				if (coordinate >= MAX_COORDINATE) {
+					System.out.println("Can't go south!");
+				} else if (coordinate < MIN_COORDINATE){
+					System.out.println("FATAL ERROR DURING MOVING SOUTH ACTION: Player is outside boundary of game!!! PlayerY coordinate is " + coordinate);
+					System.exit(0);
+				} else {
+					System.out.println("You have travelled south.");
+					coordinate++;
+				}
+				break;
+			case 'w':
+				if (coordinate <= MIN_COORDINATE) {
+					System.out.println("Can't go west!");
+				} else if (coordinate > MAX_COORDINATE){
+					System.out.println("FATAL ERROR DURING MOVING WEST ACTION: Player is outside boundary of game!!! PlayerX coordinate is " + coordinate);
+					System.exit(0);
+				} else {
+					System.out.println("You have travelled west.");
+					coordinate--;
+				}
+				break;
+			case 'e':
+				if (coordinate >= MAX_COORDINATE) {
+					System.out.println("Can't go east!");
+				} else if (coordinate < MIN_COORDINATE){
+					System.out.println("FATAL ERROR DURING MOVING EAST ACTION: Player is outside boundary of game!!! PlayerX coordinate is " + coordinate);
+					System.exit(0);
+				} else {
+					System.out.println("You have travelled east.");
+					coordinate++;
+				}
+				break;
+			default:
+		}
+	
+		return coordinate;
+	}	
+}
--- src/MovementTestSuite.java
+++ src/MovementTestSuite.java
@@ -6,49 +6,49 @@ class MovementTestSuite {
 
 	@Test
 	public void tc1() {
-		Movement classUnderTest = new Movement();
+		MovementManager classUnderTest = new MovementManager();
 		assertEquals(-3, classUnderTest.movePlayer(-3, 'n'));
 	}
 	
 	@Test
 	public void tc2() {
-		Movement classUnderTest = new Movement();
+		MovementManager classUnderTest = new MovementManager();
 		assertEquals(4, classUnderTest.movePlayer(4, 's'));
 	}
 	
 	@Test
 	public void tc3() {
-		Movement classUnderTest = new Movement();
+		MovementManager classUnderTest = new MovementManager();
 		assertEquals(-5, classUnderTest.movePlayer(-5, 'w'));
 	}
 
 	@Test
 	public void tc4() {
-		Movement classUnderTest = new Movement();
+		MovementManager classUnderTest = new MovementManager();
 		assertEquals(7, classUnderTest.movePlayer(7, 'e'));
 	}
 	
 	@Test
 	public void tc5() {
-		Movement classUnderTest = new Movement();
+		MovementManager classUnderTest = new MovementManager();
 		assertEquals(1, classUnderTest.movePlayer(2, 'n'));
 	}
 	
 	@Test
 	public void tc6() {
-		Movement classUnderTest = new Movement();
+		MovementManager classUnderTest = new MovementManager();
 		assertEquals(1, classUnderTest.movePlayer(0, 's'));
 	}
 	
 	@Test
 	public void tc7() {
-		Movement classUnderTest = new Movement();
+		MovementManager classUnderTest = new MovementManager();
 		assertEquals(2, classUnderTest.movePlayer(3, 'w'));
 	}
 	
 	@Test
 	public void tc8() {
-		Movement classUnderTest = new Movement();
+		MovementManager classUnderTest = new MovementManager();
 		assertEquals(4, classUnderTest.movePlayer(3, 'e'));
 	}
 }
